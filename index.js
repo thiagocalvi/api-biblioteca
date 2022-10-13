e6036154c2c0d49a620768cf4ab34ce39eb9bcbf
thiagocalvi <thiagohcalvi@gmail.com>
2022-10-13 10:37:02 -0300

    v.1 API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
-const port = 8080;
+const port = process.env.PORT || 8080;
 const conection = require('./database/database');
 const cors = require('cors')
 
